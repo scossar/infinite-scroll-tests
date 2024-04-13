e510dff27df479e061f54b0f18f6d6517bfb5a1d
scossar <scossar3@gmail.com>
2024-04-12 22:23:36 -0700

    find top item id

--- app/routes/infinite.tsx
+++ app/routes/infinite.tsx
@@ -1,9 +1,9 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useFetcher, useLoaderData } from "@remix-run/react";
-import { useEffect, useRef, useState } from "react";
+import { createRef, useCallback, useEffect, useRef, useState } from "react";
 
-import type { Items } from "~/utils/backend.server";
+import type { Item, Items } from "~/utils/backend.server";
 import { getItems } from "~/utils/backend.server";
 
 import debounce from "debounce";
@@ -17,6 +17,8 @@ const LIMIT = 60;
 type FetcherData = {
   items: Items;
 };
+
+type ItemRefs = HTMLDivElement[];
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const url = new URL(request.url);
@@ -32,8 +34,12 @@ export default function Infinite() {
   const fetcher = useFetcher<FetcherData>();
   const [items, setItems] = useState(data.items);
   const page = useRef(0);
-  const contentRef = useRef<HTMLDivElement>(null);
   let loading = fetcher.state === "loading";
+
+  const itemRefs = useRef<React.RefObject<HTMLDivElement>[]>([]);
+  itemRefs.current = items.map(
+    (item: Item) => itemRefs.current[Number(item.id)] ?? createRef()
+  );
 
   useEffect(() => {
     if (fetcher?.data && fetcher.data?.items) {
@@ -44,6 +50,11 @@ export default function Infinite() {
 
   const debouncedHandleScroll = debounce(
     (scrollHeight: number, scrollTop: number, clientHeight: number) => {
+      const itemId = getTopItemId(clientHeight);
+      if (itemId) {
+        console.log(`itemId: ${itemId}`);
+      }
+
       const scrolledToBottom = scrollTop + 10 + clientHeight >= scrollHeight;
       if (scrolledToBottom && fetcher.state === "idle") {
         page.current += 1;
@@ -53,12 +64,36 @@ export default function Infinite() {
     200
   );
 
+  function getTopItemId(clientHeight: number): string | null {
+    const visibleRef = itemRefs.current.find((ref) => {
+      if (ref.current) {
+        const rect = ref.current.getBoundingClientRect();
+        return rect.top >= 0 && rect.bottom <= clientHeight;
+      }
+    });
+
+    return visibleRef && visibleRef.current?.dataset?.id
+      ? visibleRef.current.dataset.id
+      : null;
+  }
+
   function handleScroll(event: React.UIEvent<HTMLDivElement>) {
     const currentTarget = event.currentTarget;
     const scrollHeight = currentTarget.scrollHeight;
     const scrollTop = currentTarget.scrollTop;
     const clientHeight = currentTarget.clientHeight;
     debouncedHandleScroll(scrollHeight, scrollTop, clientHeight);
+
+    /* const visibleRef = itemRefs.current.find((ref) => {
+      if (ref.current) {
+        const rect = ref.current.getBoundingClientRect();
+        return rect.top >= 0 && rect.bottom <= clientHeight;
+      }
+      return false;
+    });
+    if (visibleRef && visibleRef.current) {
+      console.log("Visible Item ID:", visibleRef.current.dataset.id);
+    } */
   }
 
   return (
@@ -70,12 +105,18 @@ export default function Infinite() {
       </p>
       <div className="relative">
         <div
-          className="overflow-y-scroll max-h-96 mt-6"
+          className="overflow-y-scroll max-h-96 mt-6 divide-y divide-slate-300"
           onScroll={handleScroll}
-          ref={contentRef}
         >
-          {items.map((item) => (
-            <div key={item.id}>{item.value}</div>
+          {items.map((item, index) => (
+            <div
+              key={item.id}
+              ref={itemRefs.current[Number(item.id)]}
+              data-id={item.id}
+              className="px-3 py-6"
+            >
+              {item.value}
+            </div>
           ))}
         </div>
         <div
--- app/utils/backend.server.ts
+++ app/utils/backend.server.ts
@@ -1,4 +1,4 @@
-type Item = {
+export type Item = {
   id: string;
   value: string;
 };
